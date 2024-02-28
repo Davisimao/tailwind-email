d7b4e5c5a14de8c14f543f1b2d3278c8fa84095d
Davi Simao <davisimao@MacBook-Air-de-Davi.local>
2024-02-28 17:45:22 -0300

    on build 2.0

---
+++ src/components/EmailList/index.jsx
@@ -0,0 +1,25 @@
+import email from "../../email.json"
+import EmailListItem from "./EmailListItem"
+
+
+export  default function EmailList({setOpenEmail}){
+
+  return(
+
+
+    <>
+    <div className="relative overflow-y-scroll grow">
+        {email.map(email=> 
+          (
+            <EmailListItem
+            key={email.id}
+            onClick={()=> setOpenEmail(email)} 
+            email={email}
+            />
+          
+          ))}
+    </div>
+    </>
+  )
+
+}
